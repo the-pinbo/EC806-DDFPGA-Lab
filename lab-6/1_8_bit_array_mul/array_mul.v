de8088aa70ff225d07dce20b4e7e6b58d1d99cdd
Inbasekaran <inba2002.p@gamil.com>
2023-11-15 20:56:53 +0530

    tested array mul module

---
+++ lab-6/1_8_bit_array_mul/array_mul.v
@@ -0,0 +1,17 @@
+`include "n_bit_array_mul.v"
+`include "n_bit_pipo.v"
+
+module array_mul #(
+    parameter N = 8
+) (
+    input[N-1:0] data_in,
+    input load_a, load_b, clk, clr,
+    output[2*N-1:0] product
+);
+    wire[N-1:0] a,b;
+    n_bit_pipo #(.N(N)) pipo_a(.clk(clk), .clr(clr), .load(load_a), .d(data_in), .q(a)),
+        pipo_b(.clk(clk), .clr(clr), .load(load_b), .d(data_in), .q(b));
+    wire[2*N-1:0] product;
+    n_bit_array_mul #(.N(N)) mul(a,b,product);
+
+endmodule
--- lab-6/1_8_bit_array_mul/n_bit_pipo.v
+++ lab-6/1_8_bit_array_mul/n_bit_pipo.v
@@ -1,13 +1,13 @@
 module n_bit_pipo #(
     parameter N = 8
 ) (
-    input clk, clr_n, load,
+    input clk, clr, load,
     input [N-1:0] d,
     output reg [N-1:0] q
 );
     
-    always @(posedge clk or negedge clr_n) begin
-        if(!clr_n)
+    always @(posedge clk or posedge clr) begin
+        if(clr)
             q <= 'b0;
         else if(load)
             q <= d;
